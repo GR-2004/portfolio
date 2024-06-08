67d77e0eac069b572a5e164db8610468fb84c30b
Ganesh Rana <ganeshrana6377772039@gmail.com>
2024-06-08 16:19:33 +0530

    add work experience section

--- src/components/RecentProject.tsx
+++ src/components/RecentProject.tsx
@@ -8,7 +8,7 @@ const RecentProject = () => {
     <div className="py-20" id="projects">
       <h1 className="heading">
         A small selection of{" "}
-        <span className="text-purple-100">recent projects</span>
+        <span className="text-purple-500">recent projects</span>
       </h1>
       <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10">
         {projects.map((project) => (
